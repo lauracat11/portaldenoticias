c59950cbba3d73227d169bf8d7fddf0817d67645
Anthony <awrodriguez.bc@gmail.com>
2023-03-31 16:35:07 +0200

    top-bar-done + styled components

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import second from ''
+import Modal from '../components/Modal'
 function Login() {
   return (
+    <>
     <div>Login</div>
+    {/* <Modal
+          VisibilidadModal={VisibilidadModal}
+          setVisibilidadModal={setVisibilidadModal}
+          title="Rol de los campeones"
+        >
+          <div className="mt-4 flex flex-col gap-4 relative">
+            {roles?.map((rol, i) => (
+              <Link
+                to={rol?.route}
+                key={i}
+                className="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md"
+              >
+                <div>{React.createElement(rol?.icon, { size: 20 })}</div>
+
+                <h2 className="whitespace-pre duration-500">{rol?.rol}</h2>
+              </Link>
+            ))}
+          </div>
+        </Modal> */}
+        </>
   )
 }
 
