2babc02b054edc501629550c4787cd792c644f82
Anthony <awrodriguez.bc@gmail.com>
2023-04-17 16:14:42 +0200

    cagada anto

--- frontend/src/components/Topnav.jsx
+++ frontend/src/components/Topnav.jsx
@@ -1,12 +1,24 @@
-import React,{useRef} from 'react'
+import React,{useRef,useContext,useState} from 'react'
 import {BrowserRouter as Router, Route, Routes,Navigate, Link} from 'react-router-dom'
 import {FaBars, FaTimes,FaRegNewspaper} from 'react-icons/fa'
+import {contextNews} from '../context/contextNews'
+import axios from 'axios';
+
 function Topnav() {
+    const {news,setnews,API_KEY} = useContext(contextNews)
+    const [q, setq] = useState("")
+    const searchNew = (event) =>
+    {
+        event.preventDefault()
+        var StringCall = `https://newsapi.org/v2/everything?q=${q}&apiKey=${API_KEY}`
+        axios.get(StringCall).then((result) => setnews(result.data.articles))
+    }
+
     const pages = 
     [
         {
             name: "Inicio",
-            link : "/home"
+            link : "/home",
         },
         {
             name: "Sobre Nosotros",
@@ -19,20 +31,22 @@ function Topnav() {
     ]
 
     const categories = [
-        {category: "Últimas Noticias"},
-        {category: "Negocios"},
-        {category: "Deportes"},
-        {category: "Politica"},
-        {category: "Salud"},
-        {category: "Arte"},
-        {category: "Tecnología"},
-        {category: "Mundo"}
+        {category: "Negocios",searcher: "business"},
+        {category: "Deportes",searcher: "sport"},
+        {category: "Politica",searcher: "policy"},
+        {category: "Salud",searcher: "health"},
+        {category: "Arte",searcher: "art"},
+        {category: "Tecnología",searcher: "Technology"},
+        {category: "Mundo",searcher: "World"}
 
     ]
     const navRef = useRef()
     const showNavbar = () =>{
         navRef.current.classList.toggle("responsive_nav");
     }
+    useEffect(()=>{
+        searchNew(e)
+    },[q])
   return (
     <>
     <header>
@@ -45,6 +59,7 @@ function Topnav() {
             <button className='nav-btn nav-close-btn' onClick={showNavbar}>
                 <FaTimes/>
             </button>
+            
         </nav>
         <button className='nav-btn' onClick={showNavbar}>
             <FaBars/>
@@ -56,11 +71,33 @@ function Topnav() {
     <div className='flex row py-2 bg-[#F25151] text-center text-[#eee]' style={{fontSize:"1.2vw", height: "6vh"}}>
     {
         categories?.map((categoria,i)=>(
-            <div key={i} className="vl" style={{width: "10vw"}}>
+            <div key={i} className="vl cursor-pointer" style={{width: "10vw"}} onClick={(e)=>{
+                setq(categoria.searcher)
+                searchNew(e)
+                }}>
                 {categoria.category}
             </div>
         ))
     }
+    <div class="">
+        <form>
+            <div class="px-5">
+            <div class="flex space-x-4 h-10">
+                <div class="flex rounded-md overflow-hidden">
+                <input type="text" class="w-full rounded-md rounded-r-none text-black" placeholder=' Buscar noticia' 
+                onChange={
+                    (e)=>{e.preventDefault()
+                    setq(e.target.value)
+                }}/>
+                <button class="bg-indigo-600 text-white px-6 text-lg font-semibold rounded-r-md" 
+                onClick={(e)=>{
+                    searchNew(e)
+                }}>Go</button>
+                </div>
+            </div>
+            </div>
+        </form>
+    </div>
     </div>
     {/* ////////// */}
     </>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React, { useState, useContext, useEffect } from "react";
+import {contextNews} from '../context/contextNews'
 function Home() {
+  const {news,setnews,API_KEY} = useContext(contextNews)
   return (
-    <div>Home</div>
+    <div>{news?.map((newItem,i)=>(
+      <>
+      <div key={i}>
+        <p>{newItem.author}</p>
+        <img src={newItem.urlToImage} />
+        </div> 
+      </>     
+  ))}</div>
   )
 }
 
