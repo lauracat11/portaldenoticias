12b8f23cd2d057a570c89dba4ae73a26e7ab9d6d
ArnauBrull <91909683+ArnauBrull@users.noreply.github.com>
2023-04-18 17:01:53 +0200

    traduccion login

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -16,28 +16,28 @@ function MainLoginPage() {
           <div class="form-container register-container">
             <form action="">
 
-              <h1>Register</h1>
+              <h1>Registrarse</h1>
               
-              <input type="text" placeholder="Name" required/>
+              <input type="text" placeholder="Nombre" required/>
             
-              <input type="email" placeholder="Email" required/>
+              <input type="email" placeholder="Correo" required/>
             
-              <input type="password" placeholder="Password" required/>
+              <input type="password" placeholder="Contraseña" required/>
             
-              <button>Register</button>
+              <button>Registrarse</button>
              
             </form>
           </div>
           <div class="form-container login-container">
             <form action="">
-              <h1>Login</h1>
+              <h1>Iniciar sesion</h1>
 
-              <input type="text" placeholder="Email" required/>
+              <input type="text" placeholder="Correo" required/>
             
-              <input type="password" placeholder="Password" required/>
+              <input type="password" placeholder="Contraseña" required/>
               
-              <a href="">Forgot your password?</a>
-              <button>Login</button>
+              <a href="">Has olvidado tu contraseña?</a>
+              <button>Iniciar sesion</button>
             </form>
           </div>
           <div class="overlay-container">
@@ -46,13 +46,13 @@ function MainLoginPage() {
                 <img></img>
                 <h1>Bienvenido</h1>
                 <a>Si todavía no te has registrado, puedes</a> <a>hacerlo para tener una experiencia</a> <a>personalizada y enterarte de todo lo que está pasando.</a>
-                <button onClick={triggerClick} class="btn" id="register">Register</button>
+                <button onClick={triggerClick} class="btn" id="register">Registrarse</button>
               </div>
               <div class="overlay-panel overlay-login">
                 <img></img>
                 <h1>Hola de nuevo!</h1>
                 <a></a>
-                <button onClick={triggerClick} class="btn" id="login">Login</button>
+                <button onClick={triggerClick} class="btn" id="login">Iniciar sesion</button>
               </div>
             </div>
           </div>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,31 +6,34 @@ import Modal from '../components/Modal'
 function Login() {
   const [state, setState] = useState('start')
   return (
-    <div>Login
+    <div>
+      <div>Login
       <LoginPage/>
-    </div>
-    <>
-    <div>Login</div>
-    {/* <Modal
-          VisibilidadModal={VisibilidadModal}
-          setVisibilidadModal={setVisibilidadModal}
-          title="Rol de los campeones"
-        >
-          <div className="mt-4 flex flex-col gap-4 relative">
-            {roles?.map((rol, i) => (
-              <Link
-                to={rol?.route}
-                key={i}
-                className="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md"
-              >
-                <div>{React.createElement(rol?.icon, { size: 20 })}</div>
+      </div>
+      <>
+      <div>Login</div>
+      {/* <Modal
+            VisibilidadModal={VisibilidadModal}
+            setVisibilidadModal={setVisibilidadModal}
+            title="Rol de los campeones"
+          >
+            <div className="mt-4 flex flex-col gap-4 relative">
+              {roles?.map((rol, i) => (
+                <Link
+                  to={rol?.route}
+                  key={i}
+                  className="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md"
+                >
+                  <div>{React.createElement(rol?.icon, { size: 20 })}</div>
 
-                <h2 className="whitespace-pre duration-500">{rol?.rol}</h2>
-              </Link>
-            ))}
-          </div>
-        </Modal> */}
-        </>
+                  <h2 className="whitespace-pre duration-500">{rol?.rol}</h2>
+                </Link>
+              ))}
+            </div>
+          </Modal> */}
+          </>
+    </div>
+    
   )
 }
 
