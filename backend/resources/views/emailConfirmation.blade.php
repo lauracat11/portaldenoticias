b9378c0a9ff5ea88c2305524030f28eda1563d0b
lauracat11 <92210196+lauracat11@users.noreply.github.com>
2023-04-24 16:28:37 +0200

    finish

---
+++ backend/resources/views/emailConfirmation.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mensaje enviado</title>
+</head>
+
+<body>
+    <h2>Bienvenido a la Comunidad</h2>
+    <p>¡Gracias por registrarte en Portal de Noticias Internacional! Estamos emocionados de tenerte como miembro de
+        nuestra comunidad global de noticias. Tu registro ha sido confirmado y ahora tienes acceso completo a nuestro
+        contenido exclusivo.</p>
+
+    <p>En Portal de Noticias Internacional, nuestra misión es proporcionar noticias confiables, precisas y actuales de
+        todo el mundo. Esperamos que encuentres nuestro contenido útil, informativo y entretenido.</p>
+
+    <p>Recuerda que en nuestro portal de noticias internacionales podrás leer las noticias más relevantes a nivel
+        mundial, así como conocer los sucesos más importantes del día, ver entrevistas exclusivas, reportajes especiales
+        y mucho más.</p>
+
+    <p> Si tienes alguna pregunta o necesitas ayuda para navegar por nuestra plataforma, no dudes en ponerte en contacto
+        con nuestro equipo de soporte técnico. Estamos aquí para ayudarte.</p>
+
+    <p> Gracias por unirte a nuestra comunidad de noticias internacionales y esperamos que disfrutes de todo lo que
+        Portal de Noticias Internacional tiene para ofrecer.</p>
+
+    <p> Atentamente,</p>
+
+    <p> Portal de Noticias Internacional</p>
+</body>
+
+</html>
