e902404ea51bb6b2d7e284444c58f8aa1ec1c965
ArnauBrull <91909683+ArnauBrull@users.noreply.github.com>
2023-05-09 17:11:11 +0200

    Newsletter funcional

--- frontend/src/components/ComentariosNoticia.jsx
+++ frontend/src/components/ComentariosNoticia.jsx
@@ -6,20 +6,21 @@ import React from 'react'
 const Comentarios = () => {
     return (
 
-        <div className='flex justify-center relative pt-5 '>
-            <div className='w-2/6 bg-[#2B252E]'>
-            <img src="./img/icon-256x256-removebg-preview.png" alt=""/> 
-                <div>Newsletter</div>  
-            </div>
-            <div className='border-2 p-20 grid-rows-2'>
-                <div className='text-xl'>Suscibete a nuestro newsletter</div>
-                <div className=''>Suscribete a nuestro Newsletter para no merderte nada de lo que pasa en el mundo</div>
-                <MalchimpSubscribe
-                    url= 'http://eepurl.com/iqFiqo'
-                />
-            </div>
+        // <div className='flex justify-center relative pt-5 '>
+        //     <div className='w-2/6 bg-[#2B252E]'>
+        //     <img src="./img/icon-256x256-removebg-preview.png" alt=""/> 
+        //         <div>Newsletter</div>  
+        //     </div>
+        //     <div className='border-2 p-20 grid-rows-2'>
+        //         <div className='text-xl'>Suscibete a nuestro newsletter</div>
+        //         <div className=''>Suscribete a nuestro Newsletter para no merderte nada de lo que pasa en el mundo</div>
+                
+        //     </div>
             
-        </div>
+        // </div>
+        <MalchimpSubscribe
+        url= 'https://gmail.us21.list-manage.com/subscribe/post?u=6d9ddcbc36699fa68694b2345&amp;id=0ac3139667&amp;f_id=00f1b6e1f0'
+        />
 
     )
 }
